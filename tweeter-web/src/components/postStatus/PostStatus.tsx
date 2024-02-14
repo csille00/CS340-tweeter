4ac1254430582e5f82630949b99eb13e28e4bf3d
Connor Ellis <ellisconnor88@gmail.com>
2024-02-14 13:47:15 -0700

    poststatus presenter

--- tweeter-web/src/components/postStatus/PostStatus.tsx
+++ tweeter-web/src/components/postStatus/PostStatus.tsx
@@ -1,91 +1,72 @@
 import "./PostStatus.css";
 import React, { useState } from "react";
-import { AuthToken, Status } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfoHook from "../userInfo/UserInfoHook";
+import {PostPresenter, PostStatusView} from "../../presenter/PostPresenter";
 
 const PostStatus = () => {
-  const { displayErrorMessage, displayInfoMessage, clearLastInfoMessage } =
-    useToastListener();
+  const {displayErrorMessage, displayInfoMessage, clearLastInfoMessage} = useToastListener();
 
-  const { currentUser, authToken } = useUserInfoHook();
+  const {currentUser, authToken} = useUserInfoHook();
   const [post, setPost] = useState("");
+
+  const listener: PostStatusView = {
+    setPost: setPost,
+    displayInfoMessage: displayInfoMessage,
+    displayErrorMessage: displayErrorMessage,
+    clearLastInfoMessage: clearLastInfoMessage
+  }
+
+  const [presenter] = useState(new PostPresenter(listener))
 
   const submitPost = async (event: React.MouseEvent) => {
     event.preventDefault();
+    await presenter.submitPost(authToken, post, currentUser)
+  }
 
-    try {
-      displayInfoMessage("Posting status...", 0);
-
-      let status = new Status(post, currentUser!, Date.now());
-
-      await postStatus(authToken!, status);
-
-      clearLastInfoMessage();
+    const clearPost = (event: React.MouseEvent) => {
+      event.preventDefault();
       setPost("");
-      displayInfoMessage("Status posted!", 2000);
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to post the status because of exception: ${error}`
-      );
-    }
-  };
+    };
 
-  const postStatus = async (
-    authToken: AuthToken,
-    newStatus: Status
-  ): Promise<void> => {
-    // Pause so we can see the logging out message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
+    const checkButtonStatus: () => boolean = () => !post.trim() || !authToken || !currentUser;
 
-    // TODO: Call the server to post the status
-  };
-
-  const clearPost = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setPost("");
-  };
-
-  const checkButtonStatus: () => boolean = () => {
-    return !post.trim() || !authToken || !currentUser;
-  };
-
-  return (
-    <form>
-      <div className="form-group mb-3">
+    return (
+        <form>
+          <div className="form-group mb-3">
         <textarea
-          className="form-control"
-          id="postStatusTextArea"
-          rows={10}
-          placeholder="What's on your mind?"
-          value={post}
-          onChange={(event) => {
-            setPost(event.target.value);
-          }}
+            className="form-control"
+            id="postStatusTextArea"
+            rows={10}
+            placeholder="What's on your mind?"
+            value={post}
+            onChange={(event) => {
+              setPost(event.target.value);
+            }}
         />
-      </div>
-      <div className="form-group">
-        <button
-          id="postStatusButton"
-          className="btn btn-md btn-primary me-1"
-          type="button"
-          disabled={checkButtonStatus()}
-          onClick={(event) => submitPost(event)}
-        >
-          Post Status
-        </button>
-        <button
-          id="clearStatusButton"
-          className="btn btn-md btn-secondary"
-          type="button"
-          disabled={checkButtonStatus()}
-          onClick={(event) => clearPost(event)}
-        >
-          Clear
-        </button>
-      </div>
-    </form>
-  );
-};
+          </div>
+          <div className="form-group">
+            <button
+                id="postStatusButton"
+                className="btn btn-md btn-primary me-1"
+                type="button"
+                disabled={checkButtonStatus()}
+                onClick={(event) => submitPost(event)}
+            >
+              Post Status
+            </button>
+            <button
+                id="clearStatusButton"
+                className="btn btn-md btn-secondary"
+                type="button"
+                disabled={checkButtonStatus()}
+                onClick={(event) => clearPost(event)}
+            >
+              Clear
+            </button>
+          </div>
+        </form>
+    );
+}
 
 export default PostStatus;
--- tweeter-web/src/presenter/PostPresenter.ts
+++ tweeter-web/src/presenter/PostPresenter.ts
@@ -1,42 +1,42 @@
 import {StatusService} from "../model/StatusService";
-import {Status} from "tweeter-shared";
+import {AuthToken, Status, User} from "tweeter-shared";
 
 export interface PostStatusView {
+    setPost: (post: string) => void
     displayInfoMessage: (
         message: string,
         duration: number,
         bootstrapClasses?: string
     ) => void;
     displayErrorMessage: (err: string) => void
+    clearLastInfoMessage: () => void;
 }
 
 export class PostPresenter {
     private service: StatusService;
     private view: PostStatusView;
 
-    private constructor(view: PostStatusView) {
+    public constructor(view: PostStatusView) {
         this.view = view;
         this.service = new StatusService()
     }
 
 
-    // public async submitPost () {
-    //     try {
-    //         this.view.displayInfoMessage("Posting status...", 0);
-    //
-    //         let status = new Status(post, currentUser!, Date.now());
-    //
-    //         await postStatus(authToken!, status);
-    //
-    //         clearLastInfoMessage();
-    //         setPost("");
-    //         displayInfoMessage("Status posted!", 2000);
-    //     } catch (error) {
-    //         displayErrorMessage(
-    //             `Failed to post the status because of exception: ${error}`
-    //         );
-    //     }
-    // };
+    public async submitPost (auth: AuthToken|null, post: string, currentUser: User|null) {
+        try {
+            this.view.displayInfoMessage("Posting status...", 0);
+
+            await this.service.postStatus(auth!, new Status(post, currentUser!, Date.now()));
+
+            this.view.clearLastInfoMessage();
+            this.view.setPost("");
+            this.view.displayInfoMessage("Status posted!", 2000);
+        } catch (error) {
+            this.view.displayErrorMessage(
+                `Failed to post the status because of exception: ${error}`
+            );
+        }
+    };
 
 
 }
