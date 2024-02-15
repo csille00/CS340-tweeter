c3680b642b5c5334b4ce12d0f8189752ce11daec
Connor Ellis <ellisconnor88@gmail.com>
2024-02-14 17:17:45 -0700

    moved poststatus

--- tweeter-web/src/model/StatusService.ts
+++ tweeter-web/src/model/StatusService.ts
@@ -2,16 +2,6 @@ import {AuthToken, FakeData, Status, User} from "tweeter-shared";
 
 export class StatusService {
 
-    public async postStatus (
-        authToken: AuthToken,
-        newStatus: Status
-    ): Promise<void> {
-        // Pause so we can see the logging out message. Remove when connected to the server
-        await new Promise((f) => setTimeout(f, 2000));
-
-        // TODO: Call the server to post the status
-    };
-
     public async loadMoreStoryItems (
         authToken: AuthToken,
         user: User,
--- tweeter-web/src/presenter/PostPresenter.ts
+++ tweeter-web/src/presenter/PostPresenter.ts
@@ -21,12 +21,21 @@ export class PostPresenter {
         this.service = new StatusService()
     }
 
+    public async postStatus (
+        authToken: AuthToken,
+        newStatus: Status
+    ): Promise<void> {
+        // Pause so we can see the logging out message. Remove when connected to the server
+        await new Promise((f) => setTimeout(f, 2000));
+
+        // TODO: Call the server to post the status
+    };
 
     public async submitPost (auth: AuthToken|null, post: string, currentUser: User|null) {
         try {
             this.view.displayInfoMessage("Posting status...", 0);
 
-            await this.service.postStatus(auth!, new Status(post, currentUser!, Date.now()));
+            await this.postStatus(auth!, new Status(post, currentUser!, Date.now()));
 
             this.view.clearLastInfoMessage();
             this.view.setPost("");
