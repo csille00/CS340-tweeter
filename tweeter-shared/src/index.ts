e377d1dd2f518fbcee8019d98b75aa034c17d850
Connor Ellis <ellisconnor88@gmail.com>
2024-04-01 21:18:06 -0600

    added DAtaPage object

--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -3,6 +3,7 @@ export { PostSegment, Type } from "./model/domain/PostSegment";
 export { Status } from "./model/domain/Status";
 export { User } from "./model/domain/User";
 export { AuthToken } from "./model/domain/AuthToken";
+export { DataPage } from "./model/domain/DataPage";
 
 // All classes that should be avaialble to other modules need to exported here. export * does not work when 
 // uploading to lambda. Instead we have to list each export.
