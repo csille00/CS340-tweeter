f929ea8ef0af80bd86a38a576ad8a0f370292ec0
Connor Ellis <ellisconnor88@gmail.com>
2024-04-01 21:17:54 -0600

    Added AuthTokenDAO implementation

--- tweeter-server/src/model/DAO/interface/AuthTokenDAO.ts
+++ tweeter-server/src/model/DAO/interface/AuthTokenDAO.ts
@@ -1,3 +1,8 @@
-export interface AuthTokenDAO {
+import {AuthToken} from "tweeter-shared/dist";
 
+export interface AuthTokenDAO {
+    getAuthToken(authToken: String): Promise<AuthToken | undefined>
+    insertAuthToken(authToken: AuthToken): Promise<void>
+    deleteAuthToken(authToken: string): Promise<void>
+    updateAuthToken(authToken: AuthToken): Promise<void>
 }
