6fa877fb0a2e7e3028d56289f499dea97aeee235
Connor Ellis <ellisconnor88@gmail.com>
2024-04-03 11:26:31 -0600

    status service to use real data

--- tweeter-server/src/model/service/StatusService.ts
+++ tweeter-server/src/model/service/StatusService.ts
@@ -1,19 +1,25 @@
-import {AuthToken, FakeData, Status, User} from "tweeter-shared";
+import {AuthToken, Status, User} from "tweeter-shared";
 import {FeedDAO} from "../DAO/interface/FeedDAO";
 import {DynamoFactoryDAO} from "../DAO/DynamoFactoryDAO";
 import {AuthTokenDAO} from "../DAO/interface/AuthTokenDAO";
 import {UserDAO} from "../DAO/interface/UserDAO";
+import {StoryDAO} from "../DAO/interface/StoryDAO";
+import {Service} from "./Service";
 
-export class StatusService {
+export class StatusService extends Service {
 
-    feedDAO: FeedDAO
-    authDAO: AuthTokenDAO
-    userDAO: UserDAO
+    feedDAO: FeedDAO;
+    authDAO: AuthTokenDAO;
+    userDAO: UserDAO;
+    storyDAO: StoryDAO;
+
     constructor() {
+        super()
         const factoryDAO = new DynamoFactoryDAO()
         this.feedDAO = factoryDAO.getFeedDAO()
         this.authDAO = factoryDAO.getAuthTokenDAO()
         this.userDAO = factoryDAO.getUserDAO()
+        this.storyDAO = factoryDAO.getStoryDAO()
     }
 
 
@@ -23,17 +29,9 @@ export class StatusService {
         pageSize: number,
         lastItem: Status | null
     ): Promise<[Status[], boolean]> {
-        // TODO: Replace with the result of calling server
-
-        if (user === null) {
-            throw new Error("[Bad Request] user not found");
-        }
-
-        if(authToken === null){ //change this to a real authToken check
-            throw new Error("[AuthError] invalid token")
-        }
-
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+        await this.validateAuthToken(authToken)
+        const result = await this.storyDAO.getUserStoryPage(user, lastItem, pageSize);
+        return [result.values, result.hasMorePages]
     };
 
     public async loadMoreFeedItems (
@@ -43,16 +41,8 @@ export class StatusService {
         lastItem: Status | null
     ): Promise<[Status[], boolean]> {
 
-        if (user === null) {
-            throw new Error("[Bad Request] user not found");
-        }
-
-        if(authToken === null){ //change this to a real authToken check
-            throw new Error("[AuthError] invalid token")
-        }
-
-        // TODO: Replace with the result of calling server
-        const result = await this.feedDAO.getUserFeedPage(user, lastItem, pageSize)
+        await this.validateAuthToken(authToken);
+        const result = await this.feedDAO.getUserFeedPage(user, lastItem, pageSize);
         return [result.values, result.hasMorePages]
     };
 
@@ -68,6 +58,6 @@ export class StatusService {
         const userAlias = await this.authDAO.getAuthToken(authToken.token)
         const user = this
 
-        return this.feedDAO.postFeed(newStatus)
+        // return this.feedDAO.postFeed(newStatus)
     };
 }
