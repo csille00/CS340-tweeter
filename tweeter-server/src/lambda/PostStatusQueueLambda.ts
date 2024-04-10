47b0f81985cf2221851533e585ed6179ebdbef30
Connor Ellis <ellisconnor88@gmail.com>
2024-04-09 22:52:23 -0600

    PostStatusQueueLambda function

---
+++ tweeter-server/src/lambda/PostStatusQueueLambda.ts
@@ -0,0 +1,20 @@
+// import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+
+// let sqsClient = new SQSClient();
+
+import {Status} from "tweeter-shared";
+import {StatusService} from "../model/service/StatusService";
+
+export const handler = async function (event: any) {
+    console.log(event);
+    for (let i = 0; i < event.Records.length; ++i) {
+        const { body } = event.Records[i];
+        console.log(body)
+        const status = Status.fromJson(body)
+        if(status !== null){
+            const statusService = new StatusService()
+            await statusService.postStatusQueue(status);
+        }
+    }
+    return null;
+};
